1334d5a0ba16135dfb48aa13d8ac5efe94f2b032
Admin <dance.with.me@bk.ru>
2025-11-22 12:01:41 +0700

    delete fun Greeting

--- app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
+++ app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
@@ -28,18 +28,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun ScreenSetupreview() {
     SharedFlowDemoTheme {
-        ScreenSetup
+        ScreenSetup()
     }
 }
