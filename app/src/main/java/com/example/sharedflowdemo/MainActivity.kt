cd399d27bd149fea6e9eaad0341ab2b95fa4ab57
Admin <dance.with.me@bk.ru>
2025-11-22 13:11:11 +0700

    end project

--- app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
+++ app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
@@ -19,7 +19,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sharedflowdemo.ui.theme.SharedFlowDemoTheme
 import kotlinx.coroutines.flow.SharedFlow
@@ -61,22 +63,24 @@ fun MainScreen(
     modifier: Modifier = Modifier,
     sharedFlow: SharedFlow<Int>
 ) {
-    val messages = remember { mutableStateListOf<Int>()}
+    val messages = remember { mutableStateListOf<Int>() }
     val lifecycleOwner = LocalLifecycleOwner.current
     LaunchedEffect(key1 = Unit) {
-        sharedFlow.collect {
-            println("Collecting $it")
-            messages.add(it)
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            sharedFlow.collect {
+                println("Collecting $it")
+                messages.add(it)
+            }
         }
     }
 
-    LazyColumn(modifier = modifier) {
-        items(messages) {
-            Text(
-                "Collected Value = $it",
-                style = MaterialTheme.typography.headlineLarge,
-                modifier = Modifier.padding(5.dp)
-            )
+        LazyColumn(modifier = modifier) {
+            items(messages) {
+                Text(
+                    "Collected Value = $it",
+                    style = MaterialTheme.typography.headlineLarge,
+                    modifier = Modifier.padding(5.dp)
+                )
+            }
         }
     }
-}
